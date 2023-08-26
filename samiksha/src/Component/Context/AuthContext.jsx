37614622fd0aa73d837c1138ae1d2a8cfc688fb9
samiksha-3010 <samiksha30102@gmail.com>
2023-08-26 11:15:25 +0530

    done

--- samiksha/src/Component/Context/AuthContext.jsx
+++ samiksha/src/Component/Context/AuthContext.jsx
@@ -1,109 +1,109 @@
+
 // import { createContext, useEffect, useReducer } from "react";
+
 // export const AuthContext = createContext();
-// const initalState = {user: null}
+
+// const initalState = { user: null, product: [], studentList: {} };
 
 
-// const reducer = (state,action) =>{
-//     console.log(action,"-action here")
+// const reducer = (state, action) => {
+//     console.log(action, "- action here")
 
-     
-//     switch (action.type){
+  
+
+//     switch (action.type) {
 //         case "LOGIN":
-//             return{user:action.payload}
-//             case "LOGOUT":
-//                 return {user: null}
-//                 default: 
-//                 return state;
+//             return { user: action.payload } // re assigning
+//         case "LOGOUT":
+//             return { user: null } // re assign
+//         default:
+//             return state;
+//     }
+
 // }
-// }
-// export const AuthProvider = ({children})=> {
-//     const [state,dispatch] = useReducer(reducer.initalState)
-//     function Login(userData){
+// // Higher Order Component 
+// export const AuthProvider = ({ children }) => {
+
+//     const [state, dispatch] = useReducer(reducer, initalState);
+
+//     function Login(userData) {
 //         dispatch({
-//            type:"LOGIN",
-//            payload:userData
+//             type: "LOGIN",
+//             payload: userData
 //         })
 //     }
-//    function Logout(){
-//     localStorage.removeItem("Currrent-user")
-//     dispatch({
-//         type: "LOGOUT"
-//     })
-//    }
-//    useEffect(() =>{
-//     const userData = JSON.parse(localStorage.getItem("Currrent-user"))
-
-//     if(userData){
+//     function Logout() {
+//         localStorage.removeItem("Currrent-user")
 //         dispatch({
-//            type:"LOGIN", 
-//            payload: userData
+//             type: "LOGOUT"
 //         })
 //     }
-//    },[])
-//    return(
-//     <AuthContext.Provider value={{state,Login,Logout}}>
-//         {children}
-//     </AuthContext.Provider>
-//    )
+//     useEffect(() => {
+//         const userData = JSON.parse(localStorage.getItem("Currrent-user"))
+//         // console.log(userData, "userData")
+//         if (userData) {
+//             dispatch({
+//                 type: "LOGIN",
+//                 payload: userData
+//             })
+//         }
+//     }, [])
+//     return (
+//         <AuthContext.Provider value={{ state, Login, Logout }}>
+//             {children}
+//         </AuthContext.Provider>
+//     )
 // }
 
 
 
-
- 
+import axios from "axios";
 import { createContext, useEffect, useReducer } from "react";
 
 export const AuthContext = createContext();
 
-const initalState = { user: null, product: [], studentList: {} };
-
+const initialState = { user: null };
 
 const reducer = (state, action) => {
-    console.log(action, "- action here")
-
-  
-
     switch (action.type) {
-        case "LOGIN":
-            return { user: action.payload } // re assigning
-        case "LOGOUT":
-            return { user: null } // re assign
+        case 'LOGIN':
+            return { ...state, user: action.payload }
+        case 'LOGOUT':
+            return { ...state, user: null }
         default:
-            return state;
+            return state
     }
-
 }
-// Higher Order Component 
-export const AuthProvider = ({ children }) => {
 
-    const [state, dispatch] = useReducer(reducer, initalState);
 
-    function Login(userData) {
-        dispatch({
-            type: "LOGIN",
-            payload: userData
-        })
-    }
-    function Logout() {
-        localStorage.removeItem("Currrent-user")
-        dispatch({
-            type: "LOGOUT"
-        })
-    }
+  export const AuthProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+
     useEffect(() => {
-        const userData = JSON.parse(localStorage.getItem("Currrent-user"))
-        // console.log(userData, "userData")
-        if (userData) {
-            dispatch({
-                type: "LOGIN",
-                payload: userData
-            })
+        async function getCurrentUserData() {
+            var token = JSON.parse(localStorage.getItem("token"));
+            const response = await axios.post("http://localhost:8000/get-current-user", { token });
+            if (response.data.success) {
+                dispatch({
+                    type: "LOGIN",
+                    payload: response.data.user
+                })
+            } else {
+                dispatch({
+                    type: "LOGOUT"
+                });
+            }
         }
+        getCurrentUserData();
     }, [])
+
     return (
-        <AuthContext.Provider value={{ state, Login, Logout }}>
+        <AuthContext.Provider value={{ state, dispatch }}>
             {children}
         </AuthContext.Provider>
     )
+
 }
+
+export default AuthContext;
 
