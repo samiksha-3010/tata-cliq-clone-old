d09812a41ff903e5dbb6a48aef8458abccdc6cdf
samiksha-3010 <samiksha30102@gmail.com>
2024-10-30 12:54:59 +0530

    done

--- samiksha/src/Component/Home.jsx
+++ samiksha/src/Component/Home.jsx
@@ -1,14 +1,3 @@
-// import React from 'react'
-
-// const Home = () => {
-//   return (
-//     <div>Home</div>
-//   )
-// }
-
-// export default Home
-
-
 
 import React from 'react'
 import './Home.css'
